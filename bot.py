7cfaa86481da49ea01cebef6d0913deeee30482c
juanudk <juanudkp@gmail.com>
2020-07-25 10:53:48 -0300

    Name Meaning Web Scraping

--- bot.py
+++ bot.py
@@ -1,80 +1,78 @@
-import requests  
+import requests 
 from bottle import Bottle, response, request as bottle_request
+from bs4 import BeautifulSoup as bs
 
-class BotHandlerMixin:  
+class BotHandlerMixin:
     BOT_URL = None
 
     def get_chat_id(self, data):
-        """
-        Method to extract chat id from telegram request.
-        """
         chat_id = data['message']['chat']['id']
 
         return chat_id
-
+    
     def get_message(self, data):
-        """
-        Method to extract message id from telegram request.
-        """
-        message_text = data['message']['text']
+        message_id = data['message']['text']
 
-        return message_text
-
-    def get_fromName(self, data):
-        """
-        Method to extract fisrt name from telegram request.
-        """
+        return message_id
+   
+    def get_firstName(self, data):
         first_name = data['message']['from']['first_name']
 
         return first_name
 
     def send_message(self, prepared_data):
-        """
-        Prepared data should be json which includes at least `chat_id` and `text`
-        """       
+
         message_url = self.BOT_URL + 'sendMessage'
         requests.post(message_url, json=prepared_data)
 
+    def getName(self,text):
+        stringtSize = len(text)
+        name = text[15:stringtSize]
+        return name
 
-class TelegramBot(BotHandlerMixin, Bottle):  
-    BOT_URL = 'https://api.telegram.org/bot1320348444:AAEbeKsURrNmyDjSRr-cJaV5_vQtCkTEizU/'
+    def getMeaning(self,nome):
+        request = requests.get("https://www.dicionariodenomesproprios.com.br/"+nome)
+        html = bs(request.text, "html.parser")
 
-    def __init__(self, *args, **kwargs):
+        divMeaning = html.find(id="significado")
+        meanigP = divMeaning.find_all("p")
+        Meaning  = ""
+        for x in meanigP:
+            Meaning  += x.text+"\n\n"
+        return Meaning
+
+class TelegramBot(BotHandlerMixin, Bottle):
+    BOT_URL = 'https://api.telegram.org/bot976470383:AAE5_BwXgdZjWnBzLpHWLj3XubJCZmZiXsQ/'
+
+    def __init__(self,*args,**kwargs):
         super(TelegramBot, self).__init__()
         self.route('/', callback=self.post_handler, method="POST")
-
+    
     def change_text_message(self, text):
-        if (text.lower() == 'help'):
-            text = 'discord \n youtube \n twitch \n github \n canela \n bom dia \n boa tarde'
-        elif (text.lower() == 'bom dia' or text.lower() == 'boa tarde'):
-            text = text+' '+first_name
-        elif (text.lower() == 'discord'):
-            text = 'https://discord.gg/bpcNJxy'
-        elif (text.lower() == 'youtube'):
-            text = 'https://www.youtube.com/channel/UCITdE7wFDqlNxt_aJ2nSzIQ'
-        elif (text.lower() == 'github'):
-            text = 'https://github.com/juanudk'
-        elif (text.lower() == 'twitch'):
-            text = 'https://www.twitch.tv/jovemdev'
-        elif (text.lower() == 'canela'):
-            text = 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fstd1.bebee.com%2Fbc%2F2722499281%2Fus%2F13761466%2F18d060e6%2F0%2F0%2F1%2F1%2F200&imgrefurl=https%3A%2F%2Fwww.bebee.com%2Fbr%2Fbee%2Fguilherme-carvalho-lopes&tbnid=MBOZsOJeRCsO1M&vet=12ahUKEwiIuejxl9LqAhXqK7kGHSohCKQQMygBegUIARCMAQ..i&docid=BaJpNK-FG2TZdM&w=200&h=200&q=guilherme%20carvalho%20lopes&ved=2ahUKEwiIuejxl9LqAhXqK7kGHSohCKQQMygBegUIARCMAQ'
+        if(text.lower() == 'ola'):
+            text = 'Olá'
+        elif(text.lower().index('significado de ') != -1):
+            name = self.getName(text)
+            text = self.getMeaning(name)
         else:
-            text = 'Não entendi, para ver todos os comandos digite: help'
+            text = text[::-1]
         return text
 
-    def prepare_data_for_answer(self, data):
-        global first_name
-        first_name = self.get_fromName(data)
+    def prepare_data_for_answer(self,data):
         message = self.get_message(data)
+        if(message.lower() == 'significado do meu nome'):
+            first_name = self.get_firstName(data)
+            answer = self.getMeaning(first_name)
+        else:
+            answer = self.change_text_message(message)
         chat_id = self.get_chat_id(data)
-        answer = self.change_text_message(message)
         json_data = {
             "chat_id": chat_id,
             "text": answer,
         }
 
         return json_data
-
+    
     def post_handler(self):
         data = bottle_request.json
         answer_data = self.prepare_data_for_answer(data)
@@ -82,7 +80,6 @@ class TelegramBot(BotHandlerMixin, Bottle):
 
         return response
 
-
-if __name__ == '__main__':  
+if __name__ == '__main__':
     app = TelegramBot()
     app.run(host='localhost', port=8080)
